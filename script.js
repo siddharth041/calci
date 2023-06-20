7a4bbb4e7186ced595ee6a2af48461f73078393b
ASUS <siddharth041>
2023-06-20 16:09:55 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Todo: Make M+ M- and MC functional
 let string = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
@@ -6,8 +7,14 @@ Array.from(buttons).forEach((button)=>{
             string = eval(string);
             document.querySelector('input').value = string;
         }
-        console.log(e.target);
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
+        else if(e.target.innerHTML == 'C'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
     })
 })
